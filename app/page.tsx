1e2edaefb39aebcb8e124287f6fbbdfccadf67df
RicardoMorales04 <ricardoms.ti22@utsjr.edu.mx>
2024-01-16 12:27:32 -0600

    add footer component and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,38 @@
-import Link from 'next/link'
+//import Head from 'next/head'
+import styles from "@/styles/styles.module.css"
+import Footer from '@/components/footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        <li>
-          <Link href="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/pages/test">
-            Test
-          </Link>
-        </li>
-      </ul>
-    </main>
+    <div className={styles.container}>
+      {/*<Head>
+        <title>Mi pagina web</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no"/>
+  </Head>*/}
+
+      {/*Navbar*/}
+
+      <nav className={styles.navbar}>
+        <a href="#" className={styles.navbarBrand}>Home</a>
+        <ul className={styles.navbarNav}>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Ayuda</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Sesión</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/*Contenido Principal*/}
+      <div className={styles.content}>
+        <h1>Hola mundo!</h1>
+      </div>
+
+      {/*Componente Footer*/}
+      <Footer/>
+
+    </div>
   )
 }
---
+++ app/pages/test/info.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+export default function Info(){
+    return (
+    <div>
+        Sacame de la uni karen!!
+        <Image src="/dfj.jpg" alt="Imgen" width={300} height={200}/>
+    </div>
+    )
+}
